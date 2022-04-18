69e3945cdceb9a0ceb4bc91a9197ceba4d75de14
сергей токарев <sseregatokarew@gmail.com>
2022-04-18 21:26:43 +0700

    Добавьте файлы проекта.

---
+++ REC-109/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REC_109
+{
+    class Animal
+    {
+        public string say;
+        public Animal(string _say)
+        {
+            say = _say;
+        }
+        public Animal(Animal a)
+        {
+            say = a.say;
+        }
+        public void ToTolk()
+        {
+            Console.WriteLine(say);
+        }
+    }
+}
---
+++ REC-109/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REC_109
+{
+    
+   
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Animal cat = new Animal("мяу");
+                Animal dog = new Animal("гав");
+                Animal cow = new Animal("муу");
+                Animal cock = new Animal("кококо");
+
+                List<Animal> animals = new List<Animal>() { cat, dog, cow, cock };
+                foreach (Animal a in animals)
+                {
+                    a.ToTolk();
+                }
+
+            }
+       }
+}
